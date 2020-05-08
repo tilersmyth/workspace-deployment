76a8346b1404af879e9356ceb47f5a56b6fc77b4
Tyler Smith <tylersmith@t.fios-router.home>
2020-05-08 08:54:53 -0400

    chore: github actions - workflow

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   const config = app.get(ConfigService);
 
-  console.log('sddkdsddd');
+  console.log('sddkdsd');
 
   app.enableCors({
     origin: config.get<string>('FRONTEND'),
