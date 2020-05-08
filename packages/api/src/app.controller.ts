bd82b751edcdd90b196d0f67f20ed96d2e604839
Tyler Smith <tylersmith@t.fios-router.home>
2020-05-08 16:14:47 -0400

    chore: github actions - docker

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello Chauncey Babeyyy!!';
+    return 'Hello Chauncey Babe!!';
   }
 }
